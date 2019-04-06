dfdaef161d43672b7129a47389e91923e047328b
gersonbarajas62 <gersonbarajas62@gmail.com>
2019-04-06 11:23:17 -0600

    first upload

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import Navbar from './componen/layout/Navbar.js';
+import Index from './componen/layout/Index.js';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Router>
+      <React.Fragment>
+        <Navbar />
+        <div className="container">
+        <Switch>
+        <Route exact path="/" component={index} />
+        </Switch>
+        </div>
+      </React.Fragment>
+      </Router>
     );
   }
 }
---
+++ src/componen/tracks/Tracks.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class Tracks extends Component{
+render(){
+  return(
+
+  )
+}
+}
+
+export default Tracks;
